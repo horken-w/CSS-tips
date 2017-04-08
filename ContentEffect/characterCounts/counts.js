9f6d771a8a57bc7eb279cb24274d889d4ec887e9
horkenW <horkenwong@gmail.com>
2017-04-08 10:58:57 +0800

    update text counts

---
+++ ContentEffect/characterCounts/counts.js
@@ -0,0 +1,16 @@
+(function(document, window){
+	var textarea = Array.prototype.slice.call(document.getElementsByTagName('textarea'));
+
+	var addEvent = function(action, object, callback){
+
+	}
+
+	function textRemain(domEl){
+		domEl.forEach(function(i, v){
+			var max = i.maxLength;
+
+			i.addEventListener
+		})
+	}
+	textRemain(textarea);
+})(document, window)
