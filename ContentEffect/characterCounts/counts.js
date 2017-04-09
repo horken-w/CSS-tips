4376ca84e7b0578ac34ac89ca284a7901b853986
Anami <horkenwong@gmail.com>
2017-04-10 02:17:02 +0800

    update count text feature
    update count text feature

--- ContentEffect/characterCounts/counts.js
+++ ContentEffect/characterCounts/counts.js
@@ -1,16 +1,55 @@
-(function(document, window){
+!function(document, window){
+	'use strict';
 	var textarea = Array.prototype.slice.call(document.getElementsByTagName('textarea'));
 
-	var addEvent = function(action, object, callback){
-
+	function addEvent(obj,evt,fn) {
+		if (obj.addEventListener)
+			obj.addEventListener(evt, fn, false);
+		else if (obj.attachEvent)
+			obj.attachEvent('on'+evt, fn);
 	}
 
-	function textRemain(domEl){
-		domEl.forEach(function(i, v){
-			var max = i.maxLength;
+	function validateChacter(keycode){
+		var checked = false;
 
-			i.addEventListener
-		})
+		checked = 
+		(keycode > 47 && keycode < 58)   || // number
+        keycode == 32 || keycode == 8    || // spacebar & backspace
+        (keycode > 64 && keycode < 91)   || // letter
+        (keycode > 95 && keycode < 112)  || // numpad
+        (keycode > 185 && keycode < 193) || // ;=,-./`
+        (keycode > 218 && keycode < 223);   // [\]'   
+
+		return checked;
 	}
+
+	function textCounts(evt){
+		var max = this.maxLength, isChrome = window.chrome;
+
+		if(isChrome) 
+			if(validateChacter(evt.keyCode) && this.value.length <= max)
+				this.nextElementSibling.innerText = max - this.value.replace(/\r(?!\n)|\n(?!\r)/g, "\r\n").length;
+
+		else if(validateChacter(evt.keyCode) && this.value.length <= max)
+			this.nextElementSibling.innerText = max - this.value.length;
+		// else
+		// 	this.value = this.value.slice(0, -1);
+	}
+
+	function setRemainArea(node){
+		var span = document.createElement('span'), root = node.parentElement;
+		span.classList.add('highlight');
+		span.innerText = node.maxLength;
+		root.appendChild(span);
+	}
+
+	function textRemain(domEl) {
+		domEl.forEach(function(i, v) {
+			setRemainArea(i);
+			addEvent(i, 'keyup', textCounts)
+	    })
+	}
+
+
 	textRemain(textarea);
-})(document, window)
+}(document, window)
